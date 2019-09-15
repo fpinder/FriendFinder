261f035a0bd8ff6acf3ec72c9a4f74b7fefa9402
Frank P <fpinder@comcast.net>
2019-09-15 02:50:47 -0700

    change url in friend.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     // This will be used to display a JSON of all possible friends.
     app.get('/api/friends', function (req, res) {
         res.json(friends);
-        console.log(friends)
+        //  console.log(friends)
     });
 
     //This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic
@@ -31,24 +31,24 @@ module.exports = function (app) {
             scores: userResults
         };
 
-        console.log("Name: " + userName);
-        console.log("User Score " + userScores);
+        //  console.log("Name: " + userName);
+        //  console.log("User Score " + userScores);
 
         var sum = userResults.reduce((a, b) => a + b, 0);
-        console.log("Sum of users score " + sum);
-        console.log("Best match friend difference " + bestMatch.friendDiffer);
+        //  console.log("Sum of users score " + sum);
+        //  console.log("Best match friend difference " + bestMatch.friendDiffer);
 
-        //All the friends posabilities in the data base
+        //All the friends  in the data base
         for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i].name);
+            //  console.log(friends[i].name);
             totalDifference = 0;
-            console.log("Total Diff " + totalDifference);
-            console.log("Best match friend difference " + bestMatch.friendDiffer);
+            //  console.log("Total Diff " + totalDifference);
+            // console.log("Best match friend difference " + bestMatch.friendDiffer);
 
-            var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
-            console.log("Total friend score " + bfriendScore);
+            var bfriendScore = friends[i].score.reduce((a, b) => a + b, 0);
+            //  console.log("Total friend score " + bfriendScore);
             totalDifference += Math.abs(sum - bfriendScore);
-            console.log("-------------------------> " + totalDifference);
+            //   console.log("-------------------------> " + totalDifference);
 
             //Sum the diferences and detertmine the best match for the best friend
             if (totalDifference <= bestMatch.friendDiffer) {
@@ -56,14 +56,14 @@ module.exports = function (app) {
                 bestMatch.photo = friends[i].photo;
                 bestMatch.friendDiffer = totalDifference;
             }
-            console.log(totalDifference + " Total Difference");
+            // console.log(totalDifference + " Total Difference");
         }
-        console.log(bestMatch);
+        // console.log(bestMatch);
 
         //save the data user to the database and return the JSON with the use best match 
         friends.push(userData);
-        console.log("New user added");
-        console.log(userData);
+        //  console.log("New user added");
+        //  console.log(userData);
         res.json(bestMatch);
     });
 };
