4026e06ef6382ca8a7875d0f0b31e7f2869044da
Frank P <fpinder@comcast.net>
2019-09-17 16:21:10 -0700

    updted style sheet

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,39 @@
-var friends = [{
+var friends = [
+    {
         name: "Taylor Swift",
         photo: "https://s2.r29static.com/bin/entry/6e5/720x864,85/2201206/image.webp",
-        scores: [3, 4, 5, 2, 1, 3, 4, 5, 2, 1, ]
-    }, {
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
         name: "Tyson Beckford",
         photo: "http://liverampup.com/uploads/celebrity/tyson-beckford.jpg",
-        scores: [1, 5, 4, 2, 3, 1, 5, 4, 2, 3]
-    }, {
+        scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
         name: "Margot Robbie",
         photo: "https://pixel.nymag.com/imgs/fashion/daily/2016/07/30/30-margot-robbie.w700.h700.jpg",
-        scores: [3, 5, 1, 2, 4, 3, 5, 1, 2, 4]
+        scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
     },
     {
         name: "Jennifer Lopez",
         photo: "http://www.gstatic.com/tv/thumb/persons/68580/68580_v9_bb.jpg",
-        scores: [1, 2, 5, 3, 4, 1, 2, 5, 3, 4]
+        scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
     },
     {
         name: "AJourdan Dunn",
         photo: "https://i.ytimg.com/vi/ix-hoezkBUo/maxresdefault.jpg",
-        scores: [5, 1, 3, 2, 4, 5, 1, 3, 2, 4]
+        scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
     },
     {
         name: "Naomi Campbell",
         photo: "https://secure.i.telegraph.co.uk/multimedia/archive/01921/naomi-campbell-fas_1921624i.jpg",
-        scores: [1, 2, 5, 4, 3, 1, 2, 5, 4, 3]
+        scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
     },
     {
         name: "Miranda Kerr",
         photo: "http://www.gstatic.com/tv/thumb/persons/510886/510886_v9_ba.jpg",
-        scores: [3, 2, 4, 1, 5, 3, 2, 4, 1, 5]
-    },
-]
+        scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    }
+];
 
-module.exports = friends
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,67 +1,61 @@
-var friends = require('../data/friends.js');
+var friends = require("../data/friends");
 
 module.exports = function (app) {
-    // This will be used to display a JSON of all possible friends.
-    app.get('/api/friends', function (req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
 
-    //This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic
     app.post("/api/friends", function (req, res) {
+        var totalDifference = 0;
+
         var bestMatch = {
             name: "",
             photo: "",
-            friendDiffer: 0
+            friendDifference: 1000
         };
-
-        //parse the user result to convert the user score to a number not a string
 
         var userData = req.body;
         var userName = userData.name;
         var userScores = userData.scores;
 
-        var userResults = userScores.map(function (item) {
+        var b = userScores.map(function (item) {
             return parseInt(item, 10);
         });
         userData = {
             name: req.body.name,
             photo: req.body.photo,
-            scores: userResults
+            scores: b
         };
 
         console.log("Name: " + userName);
         console.log("User Score " + userScores);
 
-        var sum = userResults.reduce((a, b) => a + b, 0);
+        var sum = b.reduce((a, b) => a + b, 0);
+
         console.log("Sum of users score " + sum);
-        console.log("Best match friend difference " + bestMatch.friendDiffer);
+        console.log("Best match friend diff " + bestMatch.friendDifference);
+        console.log("+++++++=================++++++++++");
 
-        //variable to calculate difference among users
-        var totalDifference;
-
-        //All the friends  in the data base
         for (var i = 0; i < friends.length; i++) {
             console.log(friends[i].name);
             totalDifference = 0;
             console.log("Total Diff " + totalDifference);
-            console.log("Best match friend difference " + bestMatch.friendDiffer);
+            console.log("Best match friend diff " + bestMatch.friendDifference);
 
             var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0);
             console.log("Total friend score " + bfriendScore);
             totalDifference += Math.abs(sum - bfriendScore);
             console.log("-------------------------> " + totalDifference);
 
-            //Sum the diferences and detertmine the best match for the best friend
-            if (totalDifference <= bestMatch.friendDiffer) {
+            if (totalDifference <= bestMatch.friendDifference) {
                 bestMatch.name = friends[i].name;
                 bestMatch.photo = friends[i].photo;
-                bestMatch.friendDiffer = totalDifference;
+                bestMatch.friendDifference = totalDifference;
             }
             console.log(totalDifference + " Total Difference");
         }
         console.log(bestMatch);
 
-        //save the data user to the database and return the JSON with the use best match 
         friends.push(userData);
         console.log("New user added");
         console.log(userData);
